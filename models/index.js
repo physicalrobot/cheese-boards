4034541ab812965d635731ea38aeede8e4be4062
Vikalp <kimboho@gmail.com>
2022-10-20 11:02:14 -0700

    fixed up tests

--- models/index.js
+++ models/index.js
@@ -3,11 +3,11 @@ const { User } = require('./User');
 const { Cheese } = require('./Cheese');
 
 
-Board.belongsTo(User, { as: "user" });
+Board.belongsTo(User, { as: "user" }); // one to many
 User.hasMany(Board, { as: "boards" });
 
 
-Cheese.belongsToMany(Board, { through: "cheese_boards" });
+Cheese.belongsToMany(Board, { through: "cheese_boards" }); //many to many
 Board.belongsToMany(Cheese, { through: "cheese_boards" })
 
 module.exports = {
