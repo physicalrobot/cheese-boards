b1c5ca0069bc2b294c57d48a929dbcdac8518d07
Vikalp <kimboho@gmail.com>
2022-10-20 09:09:00 -0700

    finished project, all testing up to eager loading pass

--- index.test.js
+++ index.test.js
@@ -1,13 +1,15 @@
 const { sequelize } = require('./db');
 const { User, Board, Cheese } = require('./models/index')
 
-describe('User and Board,Cheese Models', () => {
+describe('Band and Musician Models', () => {
     /**
      * Runs the code prior to all tests
      */
     beforeAll(async () => {
-
-        await sequelize.sync();
+        // the 'sync' method will create tables based on the model class
+        // by setting 'force:true' the tables are recreated each time the 
+        // test suite is run
+        await sequelize.sync({ force: true });
     })
 
     test('can create a User, Board, and Cheese', async () => {
@@ -32,45 +34,63 @@ describe('User and Board,Cheese Models', () => {
     })
 
 
-    // test('Cheese - Board', async () => {
-    //     const viku = await User.create({ name: "Viku", email: "rock@gmail.com" });
+    test('Cheese - Board', async () => {
 
-    //     const cheeseboard1 = await Board.create({ type: "wood", description: "medium", rating: 3 })
-    //     const cheeseboard2 = await Board.create({ type: "wood", description: "medium", rating: 3 })
-
-    //     await viku.addBoard(cheeseboard1)
-    //     await viku.addBoard(cheeseboard2)
-    //     let vikusboards = await viku.getBoards();
-    //     expect(vikusboards.length).toBe(2)
-
-    // })
+        const cheese = await Cheese.create({ title: "Pepper Jack", description: "spicy and good" });
+        const cheese1 = await Cheese.create({ title: "Cheddar", description: "tangy" });
 
 
-    // test('User-Board Associations', async () => {
-    //     const viku = await User.create({ name: "Viku", email: "rock@gmail.com" });
+        const cheeseboard1 = await Board.create({ type: "wood", description: "medium", rating: 3 })
+        const cheeseboard2 = await Board.create({ type: "wood", description: "medium", rating: 3 })
 
-    //     const cheeseboard1 = await Board.create({ type: "wood", description: "medium", rating: 3 })
-    //     const cheeseboard2 = await Board.create({ type: "wood", description: "medium", rating: 3 })
+        await cheeseboard1.addCheeses(cheese)
+        await cheeseboard1.addCheeses(cheese1)
+        let cheesesdisplayed = await cheeseboard1.getCheeses();
+        expect(cheesesdisplayed.length).toBe(2)
+    })
 
-    //     await viku.addBoard(cheeseboard1)
-    //     await viku.addBoard(cheeseboard2)
-    //     let vikusboards = await viku.getBoards();
-    //     expect(vikusboards.length).toBe(2)
-    // })
-
-
-
-    // test('can create Cheese', async () => {
-    //     const cheese = await Cheese.create({ title: "Pepper Jack", description: "spicy and good" });
-    //     expect(cheese.title).toBe("Pepper Jack");
-    // })
+    test('User assocation', async () => {
+        const cheese = await Cheese.create({ title: "Pepper Jack", description: "spicy and good" });
+        expect(cheese.title).toBe("Pepper Jack");
+    })
 
 
+    test('eager loading', async () => {
+        await sequelize.sync({ force: true });
+
+        const viku = await User.create({ name: "Viku", email: "rock@gmail.com" })
+        const nishu = await User.create({ name: "Nish", email: "rock@gmail.com" })
+
+        const cheese2 = await Cheese.create({ title: "Swiss", description: "spicy and good" });
+        const cheese3 = await Cheese.create({ title: "White Cheddar", description: "tangy" });
+
+        const cheeseboard1 = await Board.create({ type: "wood", description: "medium", rating: 3 })
+        const cheeseboard = await Board.findByPk(1)
+
+        await cheeseboard.addCheeses(cheese2)
+        await cheeseboard.addCheeses(cheese3)
 
 
+        const boards = await Board.findAll({
+            include: [{ model: Cheese, as: 'cheeses' }]
+        });
+
+        const cheese = await Cheese.findAll({
+            include: [{ model: Board, as: 'boards' }]
+        });
+
+        const users = await User.findAll({
+            include: [{ model: Board, as: 'boards' }]
+        });
+
+
+
+
+        expect(users.length).toBe(2)
+        expect(boards.length).toBe(1)
+        expect(cheese.length).toBe(2)
+
+        // expect(boards[0].cheeses[0].title).toBe("Swiss")
+
+    })
 })
-
-
-
-
-
